cb43ad165c05837c728a31645b72dc85457f60e2
fghwett <1058178245@qq.com>
2022-02-28 19:43:46 +0800

    从环境变量获取数据库配置

--- common/config.go
+++ common/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/taoshihan1991/imaptool/tools"
 	"io/ioutil"
+	"os"
 )
 
 type Mysql struct {
@@ -18,12 +19,22 @@ func GetMysqlConf() *Mysql {
 	var mysql = &Mysql{}
 	isExist, _ := tools.IsFileExist(MysqlConf)
 	if !isExist {
-		return mysql
+		return GetMysqlConfFromEnvironment()
 	}
 	info, err := ioutil.ReadFile(MysqlConf)
 	if err != nil {
 		return mysql
 	}
 	err = json.Unmarshal(info, mysql)
+	return mysql
+}
+
+func GetMysqlConfFromEnvironment() *Mysql {
+	var mysql = &Mysql{}
+	mysql.Server = os.Getenv("MYSQL_SERVER")
+	mysql.Port = os.Getenv("MYSQL_PORT")
+	mysql.Username = os.Getenv("MYSQL_USERNAME")
+	mysql.Password = os.Getenv("MYSQL_PASSWORD")
+	mysql.Database = os.Getenv("MYSQL_DATABASE")
 	return mysql
 }
